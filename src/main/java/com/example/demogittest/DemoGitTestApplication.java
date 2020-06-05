e487bd892e9f4bc0193c268763c2342199fdc96a
19941229xz <13657211670@sina.com>
2020-06-05 17:18:32 +0800

    我今天写了crud

--- src/main/java/com/example/demogittest/DemoGitTestApplication.java
+++ src/main/java/com/example/demogittest/DemoGitTestApplication.java
@@ -3,6 +3,10 @@ package com.example.demogittest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * test  xiong1321312312312
+ */
 @SpringBootApplication
 public class DemoGitTestApplication {
 
